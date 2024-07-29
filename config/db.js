6353b7c2ce25ec8d5fee2004de18f8e9a85b34e4
Valera Savchenko <ivalera.develop@gmail.com>
2024-07-30 02:22:22 +0300

    Some change.

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected');
+        console.log('MongoDB connected. Its good!');
     } catch (error) {
         console.error(error.message);
         process.exit(1);
