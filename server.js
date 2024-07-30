0265b90130cc467b17344c5614a2a173ac6007df
Valera Savchenko <ivalera.develop@gmail.com>
2024-07-30 04:37:06 +0300

    bugs fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: 'Content-Type,Authorization,x-auth-token',
 }));
 
 app.use(express.json());
