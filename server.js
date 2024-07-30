f94150260d8b3ba4838f38d56a4aa76c5f3f499f
Valera Savchenko <ivalera.develop@gmail.com>
2024-07-30 04:32:33 +0300

    bugs fix

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
 
-// Подключение к базе данных
-connectDB();
-
-// Настройка CORS
+// Подключение CORS
 app.use(cors({
-    origin: '*', // Разрешите доступ с вашего фронтенд-домена
-    methods: 'GET,POST,PUT,DELETE', // Разрешите необходимые методы
-    allowedHeaders: 'Content-Type,Authorization', // Разрешите необходимые заголовки
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
 }));
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
-// Определение маршрутов
+// Маршруты
 app.use('/api/auth', require('./routes/auth-routes'));
 app.use('/api/words', require('./routes/word-routes'));
+
+// Базовый маршрут для проверки
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
 
 const PORT = process.env.PORT || 5000;
 
